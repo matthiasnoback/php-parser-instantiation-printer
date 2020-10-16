bc52e5b3c3078580ff3fd7653d44f9ff2d9ab78c
Matthias Noback <matthiasnoback@gmail.com>
2020-10-16 12:02:06 +0200

    Add more examples

--- tests/Fixtures/functions.php
+++ tests/Fixtures/functions.php
@@ -4,3 +4,13 @@ function greeting(string $name): string
 {
     return 'Hi, ' . $name . '!';
 }
+
+function isItNow(): bool
+{
+    return true;
+}
+
+function passByRef(array &$array): void
+{
+    $array['foo'] = 'bar';
+}

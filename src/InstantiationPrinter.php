bb2ad09f6c1e61e1994e532e0ff5f60940a8ce5b
Matthias Noback <matthiasnoback@gmail.com>
2020-10-16 10:31:16 +0200

    Generate multiple statements at once
    Also, strip attributes

--- src/InstantiationPrinter.php
+++ src/InstantiationPrinter.php
@@ -8,19 +8,54 @@ use PhpParser\Node\Arg;
 use PhpParser\Node\Expr;
 use PhpParser\Node\Expr\Array_;
 use PhpParser\Node\Scalar\String_;
+use PhpParser\Parser;
 use PhpParser\PrettyPrinter\Standard;
 use ReflectionObject;
 use RuntimeException;
 
 final class InstantiationPrinter
 {
-    public function print(Node $node): string
+    private Parser $parser;
+    private Standard $prettyPrinter;
+
+    public function __construct(Parser $parser, Standard $prettyPrinter)
     {
-        $instantiationCodeNode = new Node\Stmt\Return_($this->createInstantiationNodeFor($node));
+        $this->parser = $parser;
+        $this->prettyPrinter = $prettyPrinter;
+    }
 
-        $prettyPrinter = new Standard();
+    public function printInstantiationCodeFor(string $code): string
+    {
+        $statements = $this->parser->parse($code);
 
-        return $prettyPrinter->prettyPrint([$instantiationCodeNode]);
+        $instantiationNodes = array_map(fn(Node\Stmt $stmt) => $this->createInstantiationNodeFor($stmt), $statements);
+
+        $wrappedInArray = $this->wrapNodesInArray($instantiationNodes);
+        $return = new Node\Stmt\Return_($wrappedInArray);
+
+        return $this->printNode($return);
+    }
+
+    public function printInstantiationNodeFor(Node $node): string
+    {
+        return $this->printNode(
+            new Node\Stmt\Return_(
+                $this->createInstantiationNodeFor($node)
+            )
+        );
+    }
+
+    public function printNode(Node $node): string
+    {
+        return $this->prettyPrinter->prettyPrint([$node]);
+    }
+
+    /**
+     * @param array<Node> $nodes
+     */
+    public function printNodes(array $nodes): string
+    {
+        return $this->prettyPrinter->prettyPrint($nodes);
     }
 
     private function createInstantiationNodeFor(Node $node): Node\Expr\New_
@@ -44,7 +79,15 @@ final class InstantiationPrinter
             if ($reflection->hasProperty($parameter->getName())) {
                 $property = $reflection->getProperty($parameter->getName());
                 $property->setAccessible(true);
-                $arguments[] = new Arg($this->createExpressionNodeForValue($property->getValue($node)));
+
+
+                $value = $property->getValue($node);
+                if ($parameter->getName() === 'attributes') {
+                    // Strip existing nodes from their attributes, which only make sense in the context of parsing the nodes
+                    $value = [];
+                }
+
+                $arguments[] = new Arg($this->createExpressionNodeForValue($value));
             }
         }
 
@@ -66,11 +109,33 @@ final class InstantiationPrinter
         if (is_int($value)) {
             return new Node\Scalar\LNumber($value);
         }
+        if (is_bool($value)) {
+            return new Expr\ConstFetch(new Node\Name($value ? 'true' : 'false'));
+        }
+        if (is_null($value)) {
+            return new Expr\ConstFetch(new Node\Name('null'));
+        }
 
         if (is_array($value)) {
-            return new Array_($value);
+            return $this->wrapNodesInArray(array_map([$this, 'createExpressionNodeForValue'], $value));
         }
 
         throw new RuntimeException('No support for type value "' . var_export($value, true) . '"');
+    }
+
+    /**
+     * @param array<Expr> $nodes
+     */
+    private function wrapNodesInArray(array $nodes): Array_
+    {
+        $isAssociativeArray = count(array_filter(array_keys($nodes), fn($key) => is_string($key))) > 0;
+
+        $items = [];
+        foreach ($nodes as $key => $node) {
+            $key = $isAssociativeArray ? new String_($key) : null;
+            $items[] = new Expr\ArrayItem($node, $key);
+        }
+
+        return new Array_($items);
     }
 }
--- tests/InstantiationPrinterTest.php
+++ tests/InstantiationPrinterTest.php
@@ -4,15 +4,26 @@ declare(strict_types=1);
 namespace InstantiationPrinter;
 
 use PhpParser\Node;
+use PhpParser\ParserFactory;
+use PhpParser\PrettyPrinter\Standard;
 use PHPUnit\Framework\TestCase;
+use RuntimeException;
+use Throwable;
 
 final class InstantiationPrinterTest extends TestCase
 {
     private InstantiationPrinter $printer;
+    private Standard $prettyPrinter;
 
     protected function setUp(): void
     {
-        $this->printer = new InstantiationPrinter();
+        $parserFactory = new ParserFactory();
+        $this->prettyPrinter = new Standard();
+
+        $this->printer = new InstantiationPrinter(
+            $parserFactory->create(ParserFactory::PREFER_PHP7),
+            $this->prettyPrinter
+        );
     }
 
     /**
@@ -20,15 +31,43 @@ final class InstantiationPrinterTest extends TestCase
      * @dataProvider nodes
      */
     public function it_prints_code_that_results_in_an_equal_object_after_running_it(
-        Node $node
+        Node $originalNode,
+        ?Node $expectedNode = null
     ): void {
-        $code = $this->printer->print($node);
+        $expectedNode = $expectedNode ?? $originalNode;
+
+        $code = $this->printer->printInstantiationNodeFor($originalNode);
+
         try {
             $recreatedNode = eval($code);
-        } catch (\Throwable $previous) {
-            throw new \RuntimeException('Could not instantiate the returned code: ' . $code, 0, $previous);
+        } catch (Throwable $previous) {
+            throw new RuntimeException('Could not execute the generated code: ' . $code, 0, $previous);
         }
-        self::assertEquals($node, $recreatedNode);
+
+        self::assertEquals($expectedNode, $recreatedNode);
+    }
+
+    /**
+     * @test
+     */
+    public function it_prints_code_that_instantiates_statements(): void
+    {
+        $originalCode = <<<'EOD'
+<?php
+echo 'foo';
+EOD;
+
+        $generatedCode = $this->printer->printInstantiationCodeFor($originalCode);
+
+        try {
+            $statements = eval($generatedCode);
+        } catch (Throwable $previous) {
+            throw new RuntimeException('Could not execute the generated code: ' . $generatedCode, 0, $previous);
+        }
+
+        $resultingCode = $this->prettyPrinter->prettyPrint($statements);
+
+        self::assertEquals($originalCode, "<?php\n" . $resultingCode);
     }
 
     /**
@@ -37,6 +76,10 @@ final class InstantiationPrinterTest extends TestCase
     public function nodes(): array
     {
         return [
+            'strip attributes' => [
+                new Node\Scalar\String_('foo', ['startLine' => 1, 'endLine' => 2, 'kind' => 3]),
+                new Node\Scalar\String_('foo', [])
+            ],
             [new Node\Scalar\String_('foo')],
             [new Node\Scalar\DNumber(0.12)],
             [new Node\Scalar\LNumber(123)],
